c5311fb8c8dc5bc9e51844d3d6c1e511c06ab9d2
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-04-13 10:51:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let selecttag = document.querySelector("#colorSelect");
+
+function removecolor(){
+    let color =selecttag.value;
+     let mainstr = selecttag.innerHTML;
+
+     mainstr = mainstr.replace(`<option>${color}</option>`,"");
+
+     selecttag.innerHTML = mainstr;
+    // console.log(mainstr);
+    
+}
